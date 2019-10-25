eadda94c5bb92de94c8d5c566590bbf028d599e6
Miguel de Barros <miguel@debarros.me>
2019-10-25 10:58:22 +0100

    added additional pool configs for mysql test case

--- src/lib/mysql.js
+++ src/lib/mysql.js
@@ -7,7 +7,10 @@ let pool
 const connect = async () => {
   try {
     pool = await mysql.createPool({
-      connectionLimit: config.DATABASE.pool.max,
+      connectionLimit: config.DATABASE.pool.max, // The maximum number of connections to create at once. (Default: 10)
+      acquireTimeout: config.DATABASE.pool.ACQUIRE_TIMEOUT_MILLIS, // The milliseconds before a timeout occurs during the connection acquisition. This is slightly different from connectTimeout, because acquiring a pool connection does not always involve making a connection. If a connection request is queued, the time the request spends in the queue does not count towards this timeout. (Default: 10000)
+      waitForConnections: true, // Determines the pool's action when no connections are available and the limit has been reached. If true, the pool will queue the connection request and call it when one becomes available. If false, the pool will immediately call back with an error. (Default: true)
+      queueLimit: 0, // The maximum number of connection requests the pool will queue before returning an error from getConnection. If set to 0, there is no limit to the number of queued connection requests. (Default: 0)
       host: config.DATABASE.connection.host,
       port: config.DATABASE.connection.port,
       user: config.DATABASE.connection.user,
